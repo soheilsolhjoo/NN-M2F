1e818f6a718791a7bd5b128273494f7249deb8bc
Soheil Solhjoo <46877039+soheilsolhjoo@users.noreply.github.com>
2024-07-31 15:15:42 +0200

    Delete NN_original_files directory

--- NN_original_files/read_data.m
+++
@@ -1,8 +0,0 @@
-function number = read_data(string)
-%read_data
-idx_temp1 = ismember(string, '=');
-idx_temp2 = find(idx_temp1,1);
-idx_temp1(1:idx_temp2) = 1;
-string(idx_temp1) = [];
-number = str2num(string);
-end
